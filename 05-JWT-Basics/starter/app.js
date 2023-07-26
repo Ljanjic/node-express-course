8397241a0daca98d4d642b86d624fdb533a6378e
Ljiljana J <ljiljajanjic@gmail.com>
2023-07-26 18:46:52 -0400

    JWT Basics

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const mainRouter = require('./routes/main')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
-app.use(express.static('./public'));
-app.use(express.json());
+app.use(express.static('./public'))
+app.use(express.json())
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+app.use('/api/v1', mainRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
@@ -22,8 +26,8 @@ const start = async () => {
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 };
 
-start();
+start()
